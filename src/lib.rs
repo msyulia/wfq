50be260f9d74310f66e882b7cfe13cd6df75d294
Julia Kowalik <priv.julia.kowalik@gmail.com>
2022-12-21 15:00:35 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,25 @@ pub use wf_queue::*;
 type NodeRef<T> = AtomicRef<Node<T>>;
 type OpDescRef<T> = AtomicRef<OpDesc<T>>;
 type Phase = i64;
+
+#[cfg(test)]
+mod tests {
+
+    #[inline(always)]
+    fn foo() {
+        println!("Weirdness")
+    }
+
+    fn execute<F: FnOnce() -> ()>(fun: F) {
+        fun()
+    }
+    #[test]
+    pub fn test_inline_function_pointer_weirdness() {
+        let v = vec![1, 2, 3];
+
+        let mut v2 = Vec::<i32>::with_capacity(3);
+        v2[0] = 1;
+        v2[1] = 2;
+        v2[2] = 3;
+    }
+}
